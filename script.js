c260180b2862f13483c2a1bdabfced5e4c675818
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-29 13:34:06 -0500

    Added audio clip, created bookmark

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+	 $("#audio")[0].play();
+
+	$('#musician-bg').hide();
+
+	$('#bookmark').hide();
+
+    setTimeout(function(){
+        $('#musician-bg').fadeIn(8000);
+    }, 500);
+
+	 setTimeout(function(){
+        $('#bookmark').fadeIn(8000);
+    }, 4000);    
+
 });
