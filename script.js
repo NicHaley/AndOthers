69f3fc8b810a87a3abc7ce8cfced634883e23990
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-29 20:47:26 -0500

    Major styling

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
-	$("#audio")[0].loop = true;
-	$("#audio")[0].play();
 
-	$('.icon').click(function() {
-		if ($("#audio")[0].paused == false) {
-		    $("#audio")[0].pause();
-		    // alert('music paused');
-		} else {
-		    $("#audio")[0].play();
-		    // alert('music playing');
-		}
-	});
+	setTimeout(function(){
+		$("#audio")[0].loop = true;
+		$("#audio")[0].play();
+
+		$('.icon').click(function() {
+			if ($("#audio")[0].paused == false) {
+			    $("#audio")[0].pause();
+			    // alert('music paused');
+			} else {
+			    $("#audio")[0].play();
+			    // alert('music playing');
+			}
+		});
+	}, 1000);
 
 	$('#musician-bg').hide();
 
@@ -27,16 +30,34 @@ $(document).ready(function() {
 
 
 	setTimeout(function(){
-		$('.poem').show();
+		$('.poem-container').show();
 
 		var divs = $('#musician-bg, #bookmark');
 	    limit = 600;
+	    toggle = false;
 
 	    $(window).on('scroll', function() {
 			var scrollPos = $(this).scrollTop();
+			var scrollPosBottom = ($(this).scrollTop() + $(window).height());
 
 		    if (scrollPos <= limit) {
 		      divs.css({ 'opacity' : (1 - scrollPos/limit) });
+			}
+
+			if ((scrollPosBottom >= $('#bottom-bound').offset().top) && toggle == false) {
+				toggle = true;
+				$('.poem-container').animate({ 
+					backgroundColor: '#191919',
+					color: 'white'
+				}, 1000);
+			}
+
+			if ((scrollPosBottom < $('#bottom-bound').offset().top) && toggle == true) {
+				toggle = false;
+				$('.poem-container').animate({ 
+					backgroundColor: 'white',
+					color: '#191919'
+				}, 1000);
 			}
 		});  
 	}, 1000);
